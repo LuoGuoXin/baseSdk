c5703c9f5264c9b373d25464598b9d91d1423a9a
jingtian <luoguoxin@linghit.com>
2022-02-18 10:57:02 +0800

    项目搭建

---
+++ lib/src/main/java/cn/androidguy/basesdk/BaseApplication.kt
@@ -0,0 +1,17 @@
+package cn.androidguy.basesdk
+
+import android.app.Application
+import com.tencent.mmkv.MMKV
+
+/**
+ * DATE : 2022/2/18
+ * @author : 景天
+ */
+class BaseApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        MMKV.initialize(this)
+    }
+
+}
